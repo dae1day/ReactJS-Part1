d700816ed69c5ac671db55fc24376d5a67996bad
Dae <juliandaeone@gmail.com>
2018-06-20 11:50:37 +1000

    update new code into React file

--- React-part2.js
+++ React-part2.js
@@ -76,7 +76,7 @@ ReactDOM.render(gooseImg, document.getElementById('app'));
 
 
 
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -89,9 +89,97 @@ function makeDoggy(e) {
 }
 
 const kitty = (
-	<img onClick={makeDoggy}
+	<img onClick={makeDoggy} //onClick inside img
 		src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg" 
 		alt="kitty" />
 );
 
 ReactDOM.render(kitty, document.getElementById('app'));
+
+
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//if.js works, because the words if and else are not injected in between JSX tags. The if statement is on the outside, and no JavaScript injection is necessary.
+
+let message;
+
+if (user.age >= drinkingAge) {
+  message = (
+    <h1>
+      Hey, check out this alcoholic beverage!
+    </h1>
+  );
+} else {
+  message = (
+    <h1>
+      Hey, check out these earrings I got at Claire's!
+    </h1>
+  );
+}
+
+ReactDOM.render(
+  message, 
+  document.getElementById('app')
+);
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+
+// if/else statement begins here:
+if (coinToss() === 'heads') {
+  img = <img src={pics.kitty} />
+} else {
+  img = <img src={pics.doggy} />
+}; 
+
+ReactDOM.render(img, document.getElementById('app'));
+
+// produce img of either cat or dog depending on coinToss function 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+// truthy falsy x? y : x
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function coinToss () {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
+};
+
+const img = <img src={pics[coinToss() === 'heads' ? 'kitty' : 'doggy' ]} />;
+
+ReactDOM.render(
+	img, 
+	document.getElementById('app')
+);
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
