10a342cdec883bd744e2474f7237961923521e29
Dae <juliandaeone@gmail.com>
2018-06-25 08:38:59 +1000

    add new code into React file 30

--- React-part30.js
+++ React-part30.js
@@ -1,1 +1,24 @@
 // react tutorial part 3
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+};
+
+ReactDOM.render(
+<MyComponentClass />,
+document.getElementById('app')
+);
+
+
+
+
+// ~~~~~~~~~~ break down
+
+// create a variable named React:
+import React from 'react';
+// evaluate this variable and get a particular, imported JavaScript object:
+React // { imported object properties here... }
