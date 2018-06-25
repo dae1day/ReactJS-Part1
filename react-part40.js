5d37e67278e94dfb159c9f5e2ccd713cc8049ec1
Dae <juliandaeone@gmail.com>
2018-06-25 12:24:26 +1000

    add new classes to react 40 file

--- react-part40.js
+++ react-part40.js
@@ -159,3 +159,52 @@ ReactDOM.render(
 	<Friend />,
   document.getElementById('app')
 );
+
+//~~~~~~~~~~~~~~~~~~~~ conditional in a render function
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TodaysPlan extends React.Component {
+  render() {
+    let task;
+    if (!apocalypse) {  //if conditional located here before the return statement
+      task = 'learn React.js'
+    } else {
+      task = 'run around'
+    }
+
+    return <h1>Today I am going to {task}!</h1>;
+  }
+}
+
+ReactDOM.render(
+	<TodaysPlan />,
+	document.getElementById('app')
+);
+
+// ~~~~~~~ tonights plan 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fiftyFifty = Math.random() < 0.5;
+// if less than 0.5 it is true if not it is false
+// New component class starts here:
+class TonightsPlan extends React.Component {
+  render() {
+    if (fiftyFifty) {
+      return <h1>Tonight I'm going out WOOO</h1>;
+    } else {
+      return <h1>Tonight I'm going to bed WOOO</h1>;
+    }
+  }
+};
+
+ReactDOM.render(
+	<TonightsPlan />,
+  document.getElementById('app')
+);
+
+
+//
