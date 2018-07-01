75ed2ab020af0e60d957b6351b3b1ea0acf509d8
Dae <juliandaeone@gmail.com>
2018-07-01 16:31:11 +1000

    modify file adding event handler

--- react-part60.js
+++ react-part60.js
@@ -184,3 +184,30 @@ ReactDOM.render(
 );
 
 
+
+
+//~~~~~~~~~~~~~~~~~~~ unsure of this event handler in component class
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from './Button';
+
+
+
+class Talker extends React.Component {
+	talk () {
+	let speech = '';
+  for (let i = 0; i < 10000; i++) {
+    speech += 'blah ';
+  }
+  alert(speech);
+}
+  
+  render() {
+    return <Button />;
+  }
+}
+
+ReactDOM.render(
+  <Talker />,
+  document.getElementById('app')
+);
